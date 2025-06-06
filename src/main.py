1163bfbdca2848e265e629635c9623c23b7cce93
bpsik <01178579@pw.edu.pl>
2025-06-06 16:40:09 +0200

    Minor training changes

--- src/main.py
+++ src/main.py
@@ -8,10 +8,11 @@ from train import train
 wandb.require("legacy-service")
 
 
-def create_sweep():
-    config_path = os.path.join(os.path.dirname(__file__), "sweep_config.yaml")
-    with open(config_path, "r") as f:
-        sweep_config = yaml.safe_load(f)
+def create_sweep(sweep_config=None):
+    if sweep_config is None:
+        config_path = os.path.join(os.path.dirname(__file__), "sweep_config.yaml")
+        with open(config_path, "r") as f:
+            sweep_config = yaml.safe_load(f)
 
     return wandb.sweep(sweep=sweep_config)
 
@@ -25,5 +26,18 @@ def run_sweep(sweep_id):
 
 
 if __name__ == "__main__":
+
+    sweep_config = {
+        "method": "grid",
+        "metric": {"name": "loss", "goal": "minimize", "summary": "minimize"},
+        "parameters": {
+            "batch_size": {"values": [32]},
+            "fc_layer_size": {"values": [64, 128, 256]},
+            "dropout": {"values": [0.1]},
+            "optimizer": {"values": ["adam", "sgd"]}
+        }
+    }
+
     sweep_id = create_sweep()
+    # sweep_id = create_sweep(sweep_config)
     run_sweep(sweep_id)
